b0743cce40493dd3ee2ef3b2c37f6c64b15d1d09
Wimukthi Manchanayaka <wimukthimanchanayakawork@gmail.com>
2026-05-01 00:21:01 +0530

    new

--- src/models/User.js
+++ src/models/User.js
@@ -48,13 +48,12 @@ const UserSchema = new mongoose.Schema({
 });
 
 // Hash password before saving
-UserSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  const salt = await bcrypt.genSalt(12);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
-
+UserSchema.pre('save', async function () {
+    if (!this.isModified('password')) return;
+  
+    const salt = await bcrypt.genSalt(12);
+    this.password = await bcrypt.hash(this.password, salt);
+  });
 // Method to compare passwords
 UserSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
